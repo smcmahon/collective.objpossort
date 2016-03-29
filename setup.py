e52d8b801beccd479bba31179cfee645451cdac9
Steve McMahon <steve@dcn.org>
2016-03-29 15:35:28 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.objpossort',
+      version=version,
+      description="Add collection sort by object position in parent",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Steve McMahon',
+      author_email='steve@dcn.org',
+      url='http://github.com/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
